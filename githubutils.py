ca6a791a5ad6a48763d6a7d73b8586f92a13419b
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-10-24 18:37:01 +0200

    Add the githubutils file from Ralph Bean @ralphbean

---
+++ githubutils.py
@@ -0,0 +1,96 @@
+""" Tools for querying github.
+
+I tried using pygithub3, but it really sucks.
+"""
+
+import requests
+
+
+def _link_field_to_dict(field):
+    """ Utility for ripping apart github's Link header field.
+    It's kind of ugly.
+    """
+
+    if not field:
+        return dict()
+
+    return dict([
+        (
+            part.split('; ')[1][5:-1],
+            part.split('; ')[0][1:-1],
+        ) for part in field.split(', ')
+    ])
+
+
+def get_repos(username, token):
+    """ username should be a string
+    token should be a tuple of username and password.
+
+    item can be one of "repos" or "orgs"
+    """
+
+    tmpl = "https://api.github.com/users/{username}/repos?per_page=100"
+    url = tmpl.format(username=username)
+    return _getter(url, token)
+
+
+def get_issues(username, repo, token):
+    """ username and repo should be strings
+    token should be a tuple of username and password.
+    """
+
+    tmpl = "https://api.github.com/repos/{username}/{repo}/issues?per_page=100"
+    url = tmpl.format(username=username, repo=repo)
+    return _getter(url, token)
+
+
+def get_comments(username, repo, number, token):
+    tmpl = "https://api.github.com/repos/{username}/{repo}/issues/" + \
+        "{number}/comments?per_page=100"
+    url = tmpl.format(username=username, repo=repo, number=number)
+    return _getter(url, token)
+
+
+def get_pulls(username, repo, token):
+    """ username and repo should be strings
+    token should be a tuple of username and password.
+    """
+
+    tmpl = "https://api.github.com/repos/{username}/{repo}/pulls?per_page=100"
+    url = tmpl.format(username=username, repo=repo)
+    return _getter(url, token)
+
+
+def _getter(url, token):
+    """ Pagination utility.  Obnoxious. """
+
+    results = []
+    link = dict(next=url)
+    while 'next' in link:
+        response = requests.get(link['next'], params=dict(access_token=token))
+
+        # And.. if we didn't get good results, just bail.
+        if response.status_code != 200:
+            raise IOError(
+                "Non-200 status code %r; %r; %r" % (
+                    response.status_code, url, response.json))
+
+        if callable(response.json):
+            # Newer python-requests
+            results += response.json()
+        else:
+            # Older python-requests
+            results += response.json
+
+        link = _link_field_to_dict(response.headers.get('link', None))
+
+    return results
+
+if __name__ == '__main__':
+    # Little test.
+    import getpass
+    username = raw_input("GitHub Username: ")
+    password = getpass.getpass()
+
+    results = get_all(username, (username, password))
+    print len(results), "repos found."
